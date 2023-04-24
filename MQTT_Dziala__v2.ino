3f50d5a868405cf363885aa547593dd6c3b24e98
Jarek <jakubowski.jm@gmail.com>
2023-04-24 11:49:08 +0200

    Change comment

--- MQTT_Dziala__v2.ino
+++ MQTT_Dziala__v2.ino
@@ -35,27 +35,16 @@ unsigned long aktualny_czas = 0;
 unsigned long zapamietany_czas = 0;
 unsigned long interwal_status = 60000UL;
 
-//deklaracja sensora DHT22
+// DHT22
 #define DHT_lazienka_PIN 2  
 #define DHTTYPE DHT22
 DHT dht_lazienka(DHT_lazienka_PIN, DHTTYPE);
-//float temp_lazienka_gora;
-//float temp_lazienka_gora_before;
-//float hum_lazienka_gora;
-//float hum_lazienka_gora_before;
 String temp_lazienka_gora;
 String temp_lazienka_gora_before;
 String hum_lazienka_gora;
 String hum_lazienka_gora_before;
 
-//#define
-//oknoSalon 1;
-//const int testSwitchPin = 5;
-//byte testSwitchState;
-//byte testSwitchStateNew;
-
-//unsigned long readTime;
-
+//network and mqtt config
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xAD };
 IPAddress ip(192, 168, 1, 105);
 IPAddress server(192, 168, 1, 100);
@@ -83,7 +72,7 @@ void reconnect()
   {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("arduinoClient", "pi", "Lasica246"))
+    if (client.connect("arduinoClient", "user", "pswd"))
     {
       Serial.println("connected");
     }
@@ -122,11 +111,6 @@ void setup()
   Serial.println(hum_lazienka_gora_before);
   send_status_DHT();
 
-  //if (client.connect("arduinoClient", "pi", "Lasica246"))
-  //{
-  //  client.publish(MQTTtopic, "Hello!", true);
-  //client.publish(test_topic, String(testSwitchState).c_str(), true);
-  //}
 }
 
 bool checkBound(float newValue, float prevValue, float maxDiff)
@@ -134,7 +118,6 @@ bool checkBound(float newValue, float prevValue, float maxDiff)
   return !isnan(newValue) &&
          (newValue < prevValue - maxDiff || newValue > prevValue + maxDiff);
 }
-
 
 void loop()
 {
@@ -185,25 +168,6 @@ void send_status_kontaktrony()
   }
   Serial.println("Send interval status");
 }
-
-//void send_status_DHT()
-//{
-//  temp_lazienka_gora = dht.readTemperature();
-//  if (temp_lazienka_gora != temp_lazienka_gora_before)
-//  {
-//    client.publish("arduino/DHT/temp", String(temp_lazienka_gora).c_str(), true);
-//    temp_lazienka_gora_before = temp_lazienka_gora;
-//    Serial.println(temp_lazienka_gora);
-//  }
-//
-//  hum_lazienka_gora = dht.readHumidity();
-//  if (hum_lazienka_gora != hum_lazienka_gora_before)
-//  {
-//    client.publish("arduino/DHT/hum", String(hum_lazienka_gora).c_str(), true);
-//    hum_lazienka_gora_before = hum_lazienka_gora;
-//    Serial.println(hum_lazienka_gora);
-//  }
-//}
 
 void send_status_DHT()
 {
